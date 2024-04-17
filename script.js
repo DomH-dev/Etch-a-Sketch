bce59cd4ddfd63e6f4397d5ee12ee71c182b50c3
Dom Hampshire <110022515+DomH-dev@users.noreply.github.com>
2024-04-17 12:03:14 +0100

    feat: added colour generation and mouseover events
    Cells are given a random rgb by a function when
    created. This is then assessed when a mouseover
    event occurs and the alpha value of the RGBA is
    increased by 0.1.

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ resetButton.addEventListener("click", () => getUserInput());
 
 let rowSize = 16;
 createGrid(rowSize);
+
+gridContainer.addEventListener("mouseover", function (e) {
+  mouseOverHighlight(e.target);
+})
+
 function getUserInput() {
   rowSize = prompt("What size grid would you like to make?");
   gridContainer.textContent = "";
@@ -33,4 +38,24 @@ function createGrid(rowSize) {
     }
     gridContainer.appendChild(rowContainer);
   }
+}
+
+function mouseOverHighlight(cell) {
+   cell.style.backgroundColor = increaseAlpha(cell.style.backgroundColor);
+}
+
+function randomRGB() {
+  const num = Math.round(0xffffff * Math.random());
+  const r = num >> 16;
+  const g = num >> 8 & 255;
+  const b = num & 255
+  return "rgb(" + r + "," + g + "," + b + ",0)";
+}
+
+function increaseAlpha(RGBA) {
+  let alphaValue = RGBA.split(",").pop().slice(0, -1);
+  let newAlpha = +alphaValue + 0.1;
+  let originalColour = RGBA.split(",").slice(0,-1);
+  let newColour = originalColour + `,${newAlpha})`;
+  return newColour;
 }
